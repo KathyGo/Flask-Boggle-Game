71d64c23399f41d9ff65743a914ecee744d7383d
Yue <cathywang428@gmail.com>
2022-02-05 21:02:24 -0600

    original

--- static/app.js
+++ static/app.js
@@ -1,75 +1,64 @@
-class BoggleGame {
-	constructor(boardname, sec = 60) {
-		this.board = $(`#${boardname}`);
-		this.sec = sec;
-		this.score = 0;
-		this.guessed = [];
-		this.myTimer();
-		$('#guess-form').on('submit', this.submitWordToServer).bind(this);
-		$('#score').text(`Your score: ${this.score}`);
+let score = 0;
+const guessed = [];
+let sec = 59;
+
+async function submitWordToServer(evt) {
+	evt.preventDefault();
+	let word = evt.target[0].value;
+	if (!word) {
+		return;
+	}
+	const res = await axios.get('http://localhost:5000/validate-word', { params: { guess: word } });
+	console.log(res.data);
+	let msg = '';
+	console.log(guessed);
+	console.log(word);
+	if (guessed.includes(word)) {
+		msg = `"${word}" already been guessed`;
+	} else {
+		if (res.data['result'] === 'ok') {
+			msg = 'You got it!';
+			guessed.push(word);
+			score = score + word.length;
+		} else if (res.data['result'] === 'not-on-board') {
+			msg = `"${word}" is not on board`;
+		} else if (res.data['result'] === 'not-word') {
+			msg = `"${word}" is not a word`;
+		}
 	}
 
-	myTimer() {
-		let intervalId = setInterval(function() {
-			this.sec = this.sec - 1;
-			$('#timer').text(`Timer: ${this.sec}s`);
+	$('#word').val('');
+	$('#msg').text(msg);
+	$('#score').text(`Your score: ${score}`);
+}
 
-			if (sec === 0) {
-				clearInterval(intervalId);
-				// $('#guess-form').off('submit', submitWordToServer);
-				alert('Time Out!');
-				$('#submit-btn').addClass('disabled');
-				updatePlayerDetails();
-			}
-		}, 1000);
+function myTimer() {
+	setTimeout(stopTimer, 60000);
+	function stopTimer() {
+		clearInterval(intervalId);
+		// $('#guess-form').off('submit', submitWordToServer);
+		alert('Time Out!');
+		$('#submit-btn').addClass('disabled');
+		updatePlayerDetails();
 	}
+	let intervalId = setInterval(function() {
+		sec = sec - 1;
+		$('#timer').text(`Timer: ${sec}s`);
+	}, 1000);
+}
 
-	async submitWordToServer(evt) {
-		evt.preventDefault();
-		let word = evt.target[0].value;
-		if (!word) {
-			return;
-		}
-		const res = await axios.get('http://localhost:5000/validate-word', { params: { guess: word } });
-		console.log(res.data);
-		let msg = '';
-		console.log(this.guessed);
-		console.log(word);
-		if (this.guessed.includes(word)) {
-			msg = `"${word}" already been guessed`;
-		} else {
-			if (res.data['result'] === 'ok') {
-				msg = 'You got it!';
-				this.guessed.push(word);
-				score = score + word.length;
-			} else if (res.data['result'] === 'not-on-board') {
-				msg = `"${word}" is not on board`;
-			} else if (res.data['result'] === 'not-word') {
-				msg = `"${word}" is not a word`;
-			}
-		}
-
-		$('#word').val('');
-		$('#msg').text(msg);
-		$('#score').text(`Your score: ${score}`);
-	}
-
-	async updatePlayerDetails() {
-		const res = await axios({
-			url: 'http://localhost:5000/update-score',
-			method: 'POST',
-			data: { score }
-		});
-		if (res.data.newRecord) {
-			$('#msg')
-				.text(`Congratulations! New Record: ${score}!`)
-				.removeClass()
-				.addClass('new_record text-center mt-1');
-			$('#best-score').text(`Best Score: ${score}`);
-		}
+async function updatePlayerDetails() {
+	const res = await axios({
+		url: 'http://localhost:5000/update-score',
+		method: 'POST',
+		data: { score }
+	});
+	if (res.data.newRecord) {
+		$('#msg').text(`Congratulations! New Record: ${score}!`).removeClass().addClass('new_record text-center mt-2');
+		$('#best-score').text(`Best Score: ${score}`);
 	}
 }
 
-document.addEventListener('DOMContentLoaded', function(evt) {
-	new BoggleGame('boggle-board', 60);
-});
+myTimer();
+$('#guess-form').on('submit', submitWordToServer);
+$('#score').text(`Your score: ${score}`);
